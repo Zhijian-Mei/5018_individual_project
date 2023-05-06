d8c724702a4bbc66bdb84e657543875fc2bf36fa
Zhijian Mei <545902915@qq.com>
2023-05-06 15:08:50 +0800

    Update data_util.py

--- data_util.py
+++ data_util.py
@@ -1,12 +1,15 @@
 from datasets import load_dataset
 
 import pandas as pd
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from tqdm import trange
 
 def get_data():
     dataset = load_dataset('multi_nli')['train']
+    dataset.set_format(type="torch", columns=["premise", "hypothesis","label"])
     print(dataset)
+    dataloader = DataLoader(dataset, batch_size=4)
+    return dataloader
 
 if __name__ == '__main__':
-    get_data()
+    train_loader = get_data()
