84156bdaf9338cbdd60f7a4acd706c8fd7e52eaa
Zhijian Mei <545902915@qq.com>
2023-05-06 19:33:33 +0800

    Update train.py

--- train.py
+++ train.py
@@ -12,16 +12,19 @@ from transformers import AutoConfig, AutoModelForTokenClassification
 from model import *
 from evaluation import *
 
-from transformers import AutoTokenizer, T5ForConditionalGeneration
+from transformers import AutoTokenizer, T5ForConditionalGeneration,BertModel
+
+
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-batch_size', type=int, default=2)
-    parser.add_argument('-gpu', type=str, default = '0')
+    parser.add_argument('-gpu', type=str, default='0')
     parser.add_argument('-mode', type=str, default='g')
     parser.add_argument('-prompt', type=int, default=0)
     args = parser.parse_args()
     return args
+
 
 if __name__ == '__main__':
     args = get_args()
@@ -45,11 +48,11 @@ if __name__ == '__main__':
 
     print('loading data')
 
-    dataset = MyDataset(tokenizer,mode =args.mode,prompt=True if args.prompt else False)
+    dataset = MyDataset(tokenizer, mode=args.mode, prompt=True if args.prompt else False)
 
     train_batch_size = args.batch_size
     eval_batch_size = args.batch_size
-    train_set,eval_set = torch.utils.data.random_split(dataset, [0.8, 0.2])
+    train_set, eval_set = torch.utils.data.random_split(dataset, [0.8, 0.2])
     train_loader = DataLoader(train_set, batch_size=train_batch_size, shuffle=False)
     eval_loader = DataLoader(eval_set, batch_size=eval_batch_size)
 
@@ -80,7 +83,7 @@ if __name__ == '__main__':
                 return_tensors="pt",
             ).to(device)
 
-            outputs = model(input_ids=input_.input_ids,attention_mask=input_.attention_mask, labels=output_.input_ids)
+            outputs = model(input_ids=input_.input_ids, attention_mask=input_.attention_mask, labels=output_.input_ids)
             loss = outputs.loss
 
             optimizer.zero_grad()
@@ -98,9 +101,11 @@ if __name__ == '__main__':
         count = 0
         model.eval()
         for i in tqdm(eval_loader,
-                      mininterval=200
+                      # mininterval=200
                       ):
             text, label = i[0], i[1]
+            print(text)
+            quit()
             input_encoding = tokenizer.batch_encode_plus(
                 text,
                 max_length=max_length,
@@ -132,7 +137,8 @@ if __name__ == '__main__':
 
         f1score = f1score / count
         print(f'f1_score: {f1score} at epoch {e}')
-        torch.save({'model': model.state_dict()}, f"checkpoint/{model_name}_epoch{e}_{'freeze' if args.freeze == 1 else 'unfreeze'}.pt")
+        torch.save({'model': model.state_dict()},
+                   f"checkpoint/{model_name}_epoch{e}_{'freeze' if args.freeze == 1 else 'unfreeze'}.pt")
         if f1score > best_f1:
             best_f1 = f1score
             torch.save({'model': model.state_dict()},
