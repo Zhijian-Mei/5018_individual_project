b92b25739851c658da59e6d085478aaf56ce7a00
Zhijian Mei <545902915@qq.com>
2023-05-07 12:04:38 +0800

    Update train.py

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name).to(device)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-4)
     # inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
     # print(inputs['input_ids'].shape)
     # outputs = model(**inputs)
