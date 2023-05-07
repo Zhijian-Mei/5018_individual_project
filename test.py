54b5b51f876d4f902584791c07be4e4c9fa00936
Zhijian Mei <545902915@qq.com>
2023-05-07 19:53:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -68,8 +68,8 @@ if __name__ == '__main__':
 
     ## load fine-tuned checkpoint
 
-    # checkpoint = torch.load('checkpoint/best_bert-base-uncased_epoch0_f1:0.65.pt')
-    # model.load_state_dict(checkpoint['model'])
+    checkpoint = torch.load('checkpoint/best_t5-small_epoch0_0.33_g_1.pt')
+    model.load_state_dict(checkpoint['model'])
 
     predicts = []
     labels = []
