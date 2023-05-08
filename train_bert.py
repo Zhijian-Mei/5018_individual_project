a15898a5f992662429ceeee91009258edbe27512
Zhijian Mei <545902915@qq.com>
2023-05-08 10:38:36 +0800

    Update train_bert.py

--- train_bert.py
+++ train_bert.py
@@ -75,9 +75,6 @@ if __name__ == '__main__':
 
             outputs = model(input_ids=input_.input_ids, attention_mask=input_.attention_mask)
             logits = outputs.logits
-            print(logits.shape)
-            print(output.shape)
-            quit()
             loss = loss_fn(logits, output)
             preds = torch.argmax(logits, dim=1).float()
             print(preds.tolist())
