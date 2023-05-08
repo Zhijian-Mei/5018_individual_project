1f12757cc93b2b4fec475a2ad7ed887cf3d26050
Zhijian Mei <545902915@qq.com>
2023-05-08 17:22:33 +0800

    Update train.py

--- train.py
+++ train.py
@@ -111,6 +111,7 @@ if __name__ == '__main__':
             outputs = model.generate(input_ids=input_.input_ids, attention_mask=input_.attention_mask)
 
             output_texts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            print(text)
             print(list(output))
             print(output_texts)
             print(round(accuracy_score(list(map(f, list(output))), list(map(f, output_texts))), 2))
