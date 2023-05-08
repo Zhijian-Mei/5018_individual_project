e735f7bf3fd3d03bde84678c451f3db4525cceda
Zhijian Mei <545902915@qq.com>
2023-05-08 17:00:54 +0800

    Update train.py

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     # model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
     model = T5ForConditionalGeneration.from_pretrained(model_name).to(device)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-4)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-3)
     # inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
     # print(inputs['input_ids'].shape)
     # outputs = model(**inputs)
