41c20cdc4122021f0062f6244f52151ddcfebc7c
Zhijian Mei <545902915@qq.com>
2023-05-08 11:31:58 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,16 +3,17 @@ from torch import nn
 
 
 class MyModel(nn.Module):
-    def __init__(self, bert, config):
+    def __init__(self, bert, config,length = 512):
         super().__init__()
         self.model = bert
         self.num_labels = 3
-        self.project = nn.Linear(config.hidden_size, self.num_labels)
+        self.project1 = nn.Linear(config.hidden_size, self.num_labels)
+        self.project0 = nn.Linear(length,1)
 
     def forward(self, text, labels=None):
-        x = self.model(text['input_ids'], text['attention_mask'])
-        print(x)
-        quit()
+        x = self.model(text['input_ids'], text['attention_mask']).last_hidden_state
+        x = x.reshape((x.shape[0],x.shape[2],x.shape[1]))
+        x = self.project0(x).squeeze()
         logits = self.project(x)
         print(logits.shape)
         print(labels.shape)
