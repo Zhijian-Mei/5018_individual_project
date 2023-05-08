d028babf03237095e8237a33ae8a1ad9fc42d70c
Zhijian Mei <545902915@qq.com>
2023-05-08 17:34:59 +0800

    Update train.py

--- train.py
+++ train.py
@@ -102,16 +102,16 @@ if __name__ == '__main__':
             outputs = model.generate(**input_)
 
             output_texts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-            print(text)
-            print(list(output))
-            print(output_texts)
-            print(round(accuracy_score(list(map(f, list(output))), list(map(f, output_texts))), 2))
+            # print(text)
+            # print(list(output))
+            # print(output_texts)
+            # print(round(accuracy_score(list(map(f, list(output))), list(map(f, output_texts))), 2))
 
             global_step += 1
 
-            if global_step % 1 == 0:
+            if global_step % 100 == 0:
                 break
-        continue
+
         model.eval()
         predicts = []
         labels = []
