015b0af717957c9fb7c5e39780f0c35bcce7c049
Zhijian Mei <545902915@qq.com>
2023-05-10 13:51:34 +0800

    Update model.py

--- model.py
+++ model.py
@@ -2,11 +2,14 @@ from torch import nn
 
 
 class MyModel(nn.Module):
-    def __init__(self, bert, config, length=512):
+    def __init__(self, bert, config, length=256):
         super().__init__()
         self.model = bert
         self.num_labels = 3
-        self.fc = nn.Linear(config.hidden_size, self.num_labels)
+        self.sequence_length = length
+        self.fc1 = nn.Linear(config.hidden_size,1)
+        self.fc2 = nn.Linear(self.sequence_length,self.num_labels)
+        # self.fc = nn.Linear(config.hidden_size, self.num_labels)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.1)
         # self.softmax = nn.LogSoftmax(dim=1)
@@ -15,9 +18,11 @@ class MyModel(nn.Module):
         output = self.model(text['input_ids'], text['attention_mask'])
         last_hidden_state = output[0]
         cls_hs = output[1]
-        print(last_hidden_state.shape)
+        x = self.fc1(last_hidden_state).squeeze()
+        x = self.fc2(x)
+        x = self.dropout(x)
+        print(x.shape)
         quit()
-        x = self.dropout(cls_hs)
         logits = self.fc(x)
 
         loss_fct = nn.CrossEntropyLoss()
