0e4873ab8f55ba70b580fc4f87ccdf6a93910e3b
Zhijian Mei <545902915@qq.com>
2023-05-11 12:47:22 +0800

    Update test_bert.py

--- test_bert.py
+++ test_bert.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
 
         preds = torch.argmax(logits, dim=1).cpu()
 
-        labels.extend(output.tolist())
+        labels.extend(list(output))
         predicts.extend(preds.tolist())
 
     labels = list(map(f, labels))
@@ -138,7 +138,7 @@ if __name__ == '__main__':
 
         preds = torch.argmax(logits, dim=1).cpu()
 
-        labels.extend(output.tolist())
+        labels.extend(list(output))
         predicts.extend(preds.tolist())
 
     labels = list(map(f, labels))
