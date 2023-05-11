c18868189fcf8dd83a538484da2078fc27e7c38f
Zhijian Mei <545902915@qq.com>
2023-05-11 17:45:39 +0800

    Update compare.py

--- compare.py
+++ compare.py
@@ -87,9 +87,7 @@ if __name__ == '__main__':
 
 
     for i in tqdm(matched_dataLoader):
-        text, output = list(i[0]), i[1]
-        print(text)
-        quit()
+        text, output = list(i[0])[0], i[1]
         labels.extend(list(output))
         input_ = tokenizer1.encode(
             text,
